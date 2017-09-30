473cf7487bd4eb3929e248eb97426e17c668ea89
Roman Birukov <r.v.birukov@yandex.ru>
2017-09-30 11:43:39 +0300

    Done

--- BankLibrary/Bank.cs
+++ BankLibrary/Bank.cs
@@ -63,6 +63,102 @@ namespace BankLibrary
 
             newAccount.Open();
         }
+
+        //добавление средств на счет
+        public void Put(decimal sum, int id)
+        {
+            T account = FindAccount(id);
+            if (account == null)
+            {
+                throw new Exception("Счет не найден");
+            }
+            account.Put(sum);
+        }
+
+        // вывод средств
+        public void Withdraw(decimal sum, int id)
+        {
+            T account = FindAccount(id);
+            if (account == null)
+            {
+                throw new Exception("Счет не найден");
+            }
+            account.Withdraw(sum);
+        }
+
+        // закрытие счета
+        public void Close(int id)
+        {
+            int index;
+            T account = FindAccount(id, out index);
+            if (account == null)
+            {
+                throw new Exception("Счет не найден");
+            }
+            account.Close();
+
+            if (accounts.Length <= 1)
+            {
+                accounts = null;
+            }
+            else
+            {
+                // уменьшаем массив счетов, удаляя из него закрытый счет
+                T[] tempAccounts = new T[accounts.Length - 1];
+                for (int i = 0, j = 0; i < accounts.Length; i++)
+                {
+                    if (i != index)
+                    {
+                        tempAccounts[j++] = accounts[i];
+                    }
+                }
+                accounts = tempAccounts;
+            }
+        }
+
+        // начисление процентов по счетам
+        public void CalculatePercentage()
+        {
+            if (accounts == null) // если массив не создан, выходим из метода
+            {
+                return;
+            }
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                T account = accounts[i];
+                account.IncrementDays();
+                account.Calculate();
+            }
+        }
+
+        // Поиск счета по Id
+        public T FindAccount(int id)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i].Id == id)
+                {
+                    return accounts[i];
+                }
+            }
+            return null;
+        }
+
+        // перегруженная версия поиска счета
+        public T FindAccount(int id, out int index)
+        {
+            for (int i = 0; i < accounts.Length; i++)
+            {
+                if (accounts[i].Id == id)
+                {
+                    index = i;
+                    return accounts[i];
+                }
+            }
+            index = -1;
+            return null;
+        }
+
     }
     // тип счета
     public enum AccountType
